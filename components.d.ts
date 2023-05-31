0345e31ad61c5638aa7e3eb4c8599027ced538dd
hjh <hjh>
2023-05-31 20:37:36 +0800

    feat(custom): 今日巡更页面显示，请求调试

--- components.d.ts
+++ components.d.ts
@@ -23,11 +23,7 @@ declare module '@vue/runtime-core' {
     VanField: typeof import('vant/es')['Field']
     VanForm: typeof import('vant/es')['Form']
     VanNavBar: typeof import('vant/es')['NavBar']
-<<<<<<< HEAD
-=======
     VanPopup: typeof import('vant/es')['Popup']
-    VanSwitch: typeof import('vant/es')['Switch']
->>>>>>> origin/test
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
   }
--- src/views/patrol/index.vue
+++ src/views/patrol/index.vue
@@ -1,13 +1,30 @@
 <template>
 	<div>
 		<NavBar title="今日巡更" right-title="巡更记录" @ClickRight="rightClick"> </NavBar>
-		patrol
+
+		<div class="rounded-b-[50px] h-[150px] w-[99%] mx-auto flex justify-center items-center" style="background-color: #409eff">
+			<div class="flex justify-center items-center">
+				<div class="bg-white rounded-md w-[90px] h-[75px] opacity-50 mr-6 text-center">
+					<div class="mt-3">6</div>
+					<div>巡更点</div>
+				</div>
+				<div class="bg-white rounded-md w-[90px] h-[75px] opacity-50 text-center">
+					<div class="mt-3">0</div>
+					<div>已巡更</div>
+				</div>
+				<div class="bg-white rounded-md w-[90px] h-[75px] opacity-50 ml-6 text-center">
+					<div class="mt-3">6</div>
+					<div>未巡更</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import NavBar from '@/components/NavBar/index.vue'
 import { useRouter } from 'vue-router'
+// import{} from '@/views/home/index.vue'
 
 const router = useRouter()
 const rightClick = () => {
