99621b68e09ee977f192231edda603ffb400b0a9
litao <2532566074@qq.com>
2023-06-06 10:16:10 +0800

    feat(custom): 工单大厅数据展示完成

--- src/api/repair/repairRecord.ts
+++ src/api/repair/repairRecord.ts
@@ -1,5 +1,11 @@
 import service from '@/utils/http'
 
+//查看报修处理表
 export function getRepairRecord(data){
     return service.get(`/repair/record/page`,data)
+}
+
+//查看报修表
+export function getRepair(data){
+    return service.get(`/repair/repair/page`, data)
 }
--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { AxiosError, InternalAxiosRequestConfig, AxiosResponse } from 'axios'
 import NProgress from 'nprogress'
 const service = axios.create({
-	baseURL: 'http://localhost:8080',
+	baseURL: 'http://localhost:8083',
 	timeout: 5000
 })
 
